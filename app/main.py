3d0c9b294cc048855044c99b49dc040a80bd3ee7
Alejandro <alejandromontano@useit.co>
2023-06-03 10:54:52 -0500

    Se añade creacion de tareas a la lista clikup

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, BackgroundTasks
 import requests
 from pydantic import BaseModel
-from utils import get_secret, get_external_urls
+from hubspot import HubSpot
+from utils import get_secret, get_external_urls_hubspot, get_external_urls_clickup
 
 
 app = FastAPI()
 hubspot_key = get_secret("HUBSPOT_ACCESS_TOKEN")
-
+clickup_key = get_secret("CLICKUP_ACCESS_TOKEN")
 
 class ContactCreateRequest(BaseModel):
     company: str
@@ -15,6 +16,11 @@ class ContactCreateRequest(BaseModel):
     lastname: str
     phone: str
     website: str
+
+class TaskCreateRequest(BaseModel):
+    name: str
+    description: str
+    priority: str
 
 
 @app.post("/contacts/")
@@ -31,9 +37,56 @@ def contacts(contact: ContactCreateRequest):
             "archived": false
         }
     """
-    url = get_external_urls('CREATE_CONTACT_HUBSPOT')
+    url = get_external_urls_hubspot('CREATE_CONTACT_HUBSPOT')
     headers = {"Authorization": f"Bearer {hubspot_key}"}
     body = contact.dict()
+
+    response = requests.post(url, json=body, headers=headers)
+
+    if response.status_code == 201:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail=response.json())
+@app.get("/contacts/")
+def get_contacts():
+    """
+        API que obtiene los contacto en hubspot
+        :param contact:
+        :return:
+    """
+    url = get_external_urls_hubspot('CREATE_CONTACT_HUBSPOT')
+    headers = {"Authorization": f"Bearer {hubspot_key}"}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail=response.json())
+@app.post("/sync_contacts/")
+async def sync_contacts(background_tasks: BackgroundTasks):
+    async def sync_contacts_task():
+        contacts = get_contacts()
+        if contacts:
+            for contact in contacts.get('results', None):
+                print(contact)
+
+
+    background_tasks.add_task(sync_contacts_task)
+
+    return {"message": "Sincronización de contactos iniciada"}
+
+@app.post("/clickup/tasks/")
+def create_task(task: TaskCreateRequest):
+    """
+        API que crea un tasks en clickup
+        :param
+        :return: 201: created
+    """
+    url = get_external_urls_clickup('CREATE_TASK_CLICKUP')
+
+    headers = {"Authorization": f"{clickup_key}"}
+    body = task.dict()
 
     response = requests.post(url, json=body, headers=headers)
 
--- app/utils.py
+++ app/utils.py
@@ -17,9 +17,27 @@ def get_secret(setting, secrets=secrets):
         error_msg = f"Definir la variable de ambiente {setting}"
         raise HTTPException(status_code=500, detail=error_msg)
 
-def get_external_urls(id):
+def get_external_urls_hubspot(id):
+    """
+    Metodo para traer las urls del servicio de hubspot
+    :param id: id de la url solicitada
+    :return: url(str) solicitada
+    """
     url = None
     base_url = 'https://api.hubapi.com/crm/v3/objects/'
     if id == 'CREATE_CONTACT_HUBSPOT':
         url = f"{base_url}contacts/"
+    return url
+
+def get_external_urls_clickup(id):
+    """
+    Metodo para traer las urls del servicio de clickup
+    :param id: id de la url solicitada
+    :return: url(str) solicitada
+    """
+    url = None
+    list_id = get_secret("CLICKUP_LIST_ID")
+    base_url = 'https://api.clickup.com/api/v2/'
+    if id == 'CREATE_TASK_CLICKUP':
+        url = f"{base_url}list/{list_id}/task/"
     return url
